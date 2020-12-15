2fd60f01fee8af09013e3cdb98c468f1e8b724f2
howard3917 <71679856+howard3917@users.noreply.github.com>
2020-12-15 16:00:08 +0800

    3D program

---
+++ 3DGame/Assets/scripts/NPC.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCdata data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+    public bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "SantaClaus")
+        {
+            playerInArea = true;
+            Dialog();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "SantaClaus")
+        {
+            playerInArea = false;
+        }
+    }
+    private void Dialog()
+    {
+        /// print(data.dialougA);
+        /// print(data.dialougA[0])
+
+        ///for  (int  i  =  0;  i  < 10;   i++)
+        //		{
+        // print("迴圈:"+i);
+        for (int i = 0; i <data.dialougA.Length; i++)
+        {
+            print(data.dialougA[i]);
+
+            }
+    }
+
+
+
+
+}
---
+++ 3DGame/Assets/scripts/NPCdata.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPC資料" , menuName = "Sou/NPC 資料")]
+public class NPCdata : ScriptableObject
+{
+    [Header("第一段對話"), TextArea(1,5)]
+    public string dialougA; 
+   [Header("第二段對話"), TextArea(1, 5)] 
+   public string dialougB;
+    [Header("第三段對話"), TextArea(1, 5)]
+   public string dialougC;
+    [Header("任務項目需求數量")]
+    public int count;
+    [Header("已經取得項目數量")]
+    public int countCurrent;
+
+}
